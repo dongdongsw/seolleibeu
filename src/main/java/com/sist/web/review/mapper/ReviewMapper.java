5393f60d1a1ce7e094d22a8573f59606f5b0862f
이수현 <dltngus13855@naver.com>
2026-01-09 17:50:37 +0900

    <이수현> review back

---
+++ src/main/java/com/sist/web/review/mapper/ReviewMapper.java
@@ -0,0 +1,11 @@
+package com.sist.web.review.mapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+import com.sist.web.vo.*;
+
+@Mapper
+@Repository
+public interface ReviewMapper {
+	
+}
