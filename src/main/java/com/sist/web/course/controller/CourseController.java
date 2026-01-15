f6c5c1f570ae856df10a6b2e02eaaef6c85075a9
KIM MINSEOK <mindory144@gmail.com>
2026-01-15 21:31:04 +0900

    <김민석> 비공개 코스 작성자 아니면 403처리

--- src/main/java/com/sist/web/course/controller/CourseController.java
+++ src/main/java/com/sist/web/course/controller/CourseController.java
@@ -3,15 +3,19 @@ package com.sist.web.course.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.sist.web.course.service.CourseService;
 import com.sist.web.vo.CourseVO;
 import com.sist.web.vo.PlaceVO;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -46,9 +50,17 @@ public class CourseController {
 	
 	// 코스 게시판 상세보기 페이지 이동
 	@GetMapping("/course/detail")
-	public String course_detail(@RequestParam("cno") int cno, Model model) {
+	public String course_detail(@RequestParam("cno") int cno, Model model, HttpSession session) {
 		
 		CourseVO vo=cService.courseDetailData(cno);
+		
+		// 비공개 코스일 경우, 본인 제외 접근 차단
+		Integer uno=(Integer)session.getAttribute("uno");
+		if(vo.getIs_public().equals("N")) {
+			if((uno == null || uno != vo.getUno())) {
+				throw new ResponseStatusException(HttpStatus.FORBIDDEN, "비공개 코스 입니다."); 
+			}
+		}
 		
 		String[] pnos=vo.getPnos().split(",");
 		int firstPlace=Integer.parseInt(pnos[0]);
