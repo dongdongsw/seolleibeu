0040a1ce86911dd95162b988f1affa80a1d61c40
jieunpyo <jieunhye1121@naver.com>
2026-01-23 17:50:12 +0900

    <서동현> 즐겨찾기 수정

--- src/main/resources/static/vuejs/favorite/favoriteStore.js
+++ src/main/resources/static/vuejs/favorite/favoriteStore.js
@@ -28,7 +28,7 @@ const useFavoriteStore = defineStore('favorite',{
 		async favoriteCancel(pno){
 			const deleteMsg = confirm('즐겨찾기를 해제 하시겠습니까?')
 				if(deleteMsg){
-					const result = await api.post('favorite/cancel_vue/',{
+					const result = await api.post('/favorite/cancel_vue/',{
 						pno:pno
 					})
 					this.checkFavorite = 0
